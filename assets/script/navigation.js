d6949eb1bc3a9029eb1fd92d6812aa4683055063
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-06-29 11:37:57 +0700

    create navigation bar with responsive view

---
+++ assets/script/navigation.js
@@ -0,0 +1,17 @@
+function sideNavHandler() {
+    let navRight = document.getElementById("nav-right")
+    let navLeft = document.getElementById("nav-left")
+    let navMenu = document.getElementById("nav-menu")
+    let nav = document.getElementById("nav")
+    if (navRight.className === "nav-right") {
+        navRight.className += " open"
+        navLeft.className += " open"
+        nav.className += " open"
+        navMenu.innerText = "-"
+    } else {
+        navRight.className = "nav-right"
+        navLeft.className = "nav-left"
+        nav.className = "nav"
+        navMenu.innerText = "+"
+    }
+}
